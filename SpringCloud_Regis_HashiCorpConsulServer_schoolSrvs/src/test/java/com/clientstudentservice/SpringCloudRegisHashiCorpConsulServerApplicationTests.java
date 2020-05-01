90d7c04ec8724868a3f92a463532b07c0ee7b8e3
manish sharma <manish.sharma23@gmail.com>
2020-05-01 15:55:57 +0530

    create

---
+++ SpringCloud_Regis_HashiCorpConsulServer_schoolSrvs/src/test/java/com/clientstudentservice/SpringCloudRegisHashiCorpConsulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientstudentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRegisHashiCorpConsulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
